13d8fab3d438f3ce5b4a133d3d71e27a59131b94
Santiago Benitez Lopez <sbenitez1607@gmail.com>
2023-11-16 10:25:50 -0500

    Se agregan las validaciones en docente y publicacion, falta reglas de negocio docente

--- src/main/java/co/edu/unicauca/asae/cleanarquitecture/infraestructura/input/controllerGestionDocente/DTOPeticion/DocenteDTOPeticion.java
+++ src/main/java/co/edu/unicauca/asae/cleanarquitecture/infraestructura/input/controllerGestionDocente/DTOPeticion/DocenteDTOPeticion.java
@@ -1,14 +1,23 @@
 package co.edu.unicauca.asae.cleanarquitecture.infraestructura.input.controllerGestionDocente.DTOPeticion;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.Setter;
 @Getter
 @Setter
 public class DocenteDTOPeticion extends PersonaDTOPeticion {
 
+    @NotEmpty(message = "{docente.correo.empty}")
+    @Email(message="{docente.correo.email}")
     private String correo;
+    //TODO:
     private String vinculacion;
+    @NotEmpty(message = "{docente.departamento.empty}")
     private String departamento;
+    @NotEmpty(message = "{docente.direccion.empty}")
     private DireccionDTOPeticion objDireccion;
 
     public DocenteDTOPeticion(int idPersona,String tipoIdentificacion,String numeroIdentificacion,
--- src/main/java/co/edu/unicauca/asae/cleanarquitecture/infraestructura/input/controllerGestionPublicacion/DTOPeticion/PublicacionDTOPeticion.java
+++ src/main/java/co/edu/unicauca/asae/cleanarquitecture/infraestructura/input/controllerGestionPublicacion/DTOPeticion/PublicacionDTOPeticion.java
@@ -1,14 +1,23 @@
 package co.edu.unicauca.asae.cleanarquitecture.infraestructura.input.controllerGestionPublicacion.DTOPeticion;
 
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class PublicacionDTOPeticion {
+    @Min(value = 1, message = "{publicacion.id.min}")
     private int idPublicacion;
+    @NotEmpty(message = "{publicacion.peticion.empty}")
     private TipoDTOPeticion objTipo;
+    @NotEmpty(message="{publicacion.titulo.empty}")
     private String titulo;
+    @NotEmpty(message = "{publicacion.area.empty}")
     private String area;
 
     public PublicacionDTOPeticion(){
